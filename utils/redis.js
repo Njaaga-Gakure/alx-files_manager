f0455659ce4286e0e674eedc91df7360164a70a5
Njaaga-Gakure <njaagagakure@gmail.com>
2023-09-29 07:07:48 +0000

    Create a RedisClient class that creates a redis client, sets a key to a value, gets a key using a value and deletes a value using a key

---
+++ utils/redis.js
@@ -0,0 +1,54 @@
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.connected = false;
+    this.client.on('connect', () => {
+      console.log('Redis client connected to the server');
+      this.connected = true;
+    })
+    .on('error', (err) => {
+      console.log(`Redis client not connected to the server: ${err}`);
+    })
+  }
+
+  isAlive() {
+    return this.connected;
+  }
+
+  async get(key) {
+    const getAsync = promisify(this.client.get).bind(this.client);
+    const value = await getAsync(key);
+    return value;
+  }
+
+  async set(key, value, duration) {
+    try {
+      const setAsync = promisify(this.client.set).bind(this.client);
+      const expireAsync = promisify(this.client.expire).bind(this.client);
+      await setAsync(key, value);
+      await expireAsync(key, duration);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async del(key) {
+    try {
+      const delAsync = promisify(this.client.del).bind(this.client);
+      const deleteCount = delAsync(key);
+      if (deleteCount === 1) {
+        console.log(`${key} deleted successfully`);
+      } else {
+        console.log(`${key} not found`);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
